57a48fadd71bf4017d9bdfe8a045bc44e320eeb2
Jabba Laci <jabba.laci@gmail.com>
2025-03-29 10:01:23 +0100

    Reset state first.

--- uv_venv.py
+++ uv_venv.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """
-The command "uv venv" creates .venv/ in the project folder.
+The command "uv venv" creates `.venv/` in the project folder.
 
-This script creates the virt. env. in a separate folder (~/.virtualenvs),
-and in the project folder it creates a symbolic link called .venv that points
-on the virt. env. located in ~/.virtualenvs .
+This script creates the virt. env. in a separate folder (`~/.virtualenvs`),
+and in the project folder it creates a symbolic link called `.venv` that points
+on the virt. env. located in `~/.virtualenvs`.
 
 Tested under Linux only.
 
@@ -15,15 +15,16 @@ Author: Laszlo Szathmary (jabba.laci@gmail.com), 2024--2025
 import base64
 import hashlib
 import os
+import shutil
 import sys
 
-# DRY_RUN = True  # Don't actually execute any commands. Just print them.
-DRY_RUN = False  # Execute commands.
+SYNC = True
 
-VIRTUALENVS_BASE = os.path.join(os.path.expanduser("~"), ".virtualenvs")
+VIRTUALENVS_BASE: str = os.path.join(os.path.expanduser("~"), ".virtualenvs")
+CWD: str = os.getcwd()
 
 
-def generate_poetry_hash(path):
+def generate_poetry_hash(path: str) -> str:
     """
     Using the same hashing alg. as poetry.
     """
@@ -32,7 +33,7 @@ def generate_poetry_hash(path):
     return b64[:8]
 
 
-def generate_venv_name(project_path):
+def generate_venv_name(project_path: str = CWD) -> str:
     """
     The generated name is similar to poetry's.
     """
@@ -44,38 +45,78 @@ def generate_venv_name(project_path):
     return venv_name
 
 
-def execute(cmd):
+def execute(cmd: str) -> None:
     print("$", cmd)
-    if not DRY_RUN:
-        os.system(cmd)
+    os.system(cmd)
 
 
-def main():
-    project_path = os.getcwd()
+def delete_current_venv() -> None:
+    if os.path.isdir(".venv"):
+        try:
+            shutil.rmtree(".venv")
+            print("Successfully deleted .venv/ and all its contents")
+        except OSError as e:
+            print(f"Error: .venv/ : {e.strerror}")
+            sys.exit(1)
+    else:
+        try:
+            os.unlink(".venv")
+            print("Successfully deleted .venv")
+        except OSError as e:
+            print(f"Error: .venv : {e.strerror}")
+            sys.exit(1)
 
-    venv_name = generate_venv_name(project_path)
-    # print(f"Generated Poetry-like virt. env. name: {venv_name}")
-    # print(venv_name)
 
+def check_current_state_and_reset() -> None:
+    # ~/.virtualenvs must exist
     if not os.path.isdir(dname := VIRTUALENVS_BASE):
         print(f"Error: the folder {dname} doesn't exist")
         print("Tip: create it")
-        exit(1)
-
-    venv_folder = os.path.join(VIRTUALENVS_BASE, venv_name)
-    if os.path.isdir(venv_folder):
-        print(f"Poetry-like virt. env. already exists: {venv_folder}")
-        print("Tip: remove it first to generate a new one")
-        exit(1)
-    if os.path.exists(".venv"):
+        sys.exit(1)
+    # if ~/.virtualenvs exists:
+    venv_folder_path = os.path.join(VIRTUALENVS_BASE, generate_venv_name())
+    if os.path.isdir(venv_folder_path):
+        print(f"Poetry-like virt. env. already exists: {venv_folder_path}")
+        answer = input("Do you want to delete it [yN]? ").strip().lower()
+        if answer == "y":
+            try:
+                shutil.rmtree(venv_folder_path)
+                print(f"Successfully deleted {venv_folder_path} and all its contents")
+            except OSError as e:
+                print(f"Error: {venv_folder_path} : {e.strerror}")
+                sys.exit(1)
+        else:
+            print("abort")
+            sys.exit(1)
+        #
+    # if ~/.virtualenvs/<venv_folder> doesn't exist
+    # ./.venv mustn't exist
+    if os.path.exists(".venv") or os.path.islink(".venv"):
         print(".venv already exists in the current folder")
-        print("Tip: remove it first to generate a new one")
-        exit(1)
-    # print("#", venv_folder)
-    cmd = f"uv venv {venv_folder}"
+        answer = input("Do you want to delete it [yN]? ").strip().lower()
+        if answer == "y":
+            delete_current_venv()
+        else:
+            print("abort")
+            sys.exit(1)
+        #
+    #
+
+
+def main() -> None:
+    venv_folder_path = os.path.join(VIRTUALENVS_BASE, generate_venv_name())
+
+    check_current_state_and_reset()
+    assert not os.path.exists(venv_folder_path)
+    assert not os.path.exists(".venv")
+
+    cmd = f"uv venv '{venv_folder_path}'"
     execute(cmd)
-    cmd = f"ln -s {venv_folder} .venv"
+    cmd = f"ln -s '{venv_folder_path}' .venv"
     execute(cmd)
+    if SYNC:
+        cmd = "uv sync"
+        execute(cmd)
 
 
 if __name__ == "__main__":
